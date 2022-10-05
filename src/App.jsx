6650c4d3fe23d3f329a26dd4f1c09e499d34bb64
Gustavo <gustavo.prog40@gmail.com>
2022-10-05 17:31:22 -0300

    new layout

--- src/App.jsx
+++ src/App.jsx
@@ -2,53 +2,62 @@ import { useState } from 'react'
 import api from './api'
 import { useEffect } from 'react'
 import { Player } from 'video-react'
-
-
+import moment from 'moment'
 
 function App() {
   const [posts, setPosts] = useState([])
 
-async  function getPosts() {
-  const { data } = await api.get('/all')
-  
-  setPosts(data)
-}
-  
+  function getDateWithoutTime(date) {
+    return moment(date).format('DD-MM-YYYY')
+  }
+
+  async function getPosts() {
+    const { data } = await api.get('/all')
+
+    setPosts(data)
+    return data
+  }
+
   console.log(posts)
-  
+
   useEffect(() => {
     getPosts()
-  },[])
+  }, [])
 
   return (
-    <div style={{
-      display: 'flex', flexDirection: 'column',
-      justifyContent: 'center', alignItems: 'center'
-    }}>
-      {posts.map(item => {
+    <div
+      style={{
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'center',
+        alignItems: 'center',
+      }}
+    >
+      {posts.map((item) => {
         return (
+          <div
+            key={item.id}
+            style={{
+              width: '50%',
+            }}
+          >
+            <div style={{ width: '35%' }}>
+              <Player>
+                <source src={`https://freela-api-blog.herokuapp.com/files/${item.image}`} />
 
-          <div key={item.id} style={{
-            width: '50%',
-          }}>
-            <div style={{width:'35%'}}>
-            <Player >
-              <source src={`https://freela-api-blog.herokuapp.com/files/${item.image}`} />
+                {/* <source src={`http://localhost:5000/files/${item.image}`} /> */}
               </Player>
-              </div >
+            </div>
 
-          <p>
-            {item.title}
-          </p>
+            <p>
+              <h2>{item.title}</h2>
+            </p>
 
-          <p>
-            {item.text}
-          </p>
+            <p>{item.text}</p>
 
+            <p>Data: {getDateWithoutTime(item.createdAt)}</p>
           </div>
         )
-
-
       })}
     </div>
   )
